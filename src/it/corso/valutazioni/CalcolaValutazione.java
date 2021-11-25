c6d1db7339cac86a1b53e94349a7d4f019650f6c
sunilsuraci <suracisunil@gmail.com>
2021-11-25 16:32:29 +0100

    correzione

--- src/it/corso/valutazioni/CalcolaValutazione.java
+++ src/it/corso/valutazioni/CalcolaValutazione.java
@@ -6,21 +6,25 @@ public class CalcolaValutazione {
 	
 		int idStudente;
 		int percentualeAssenze;
-		float mediaVoti;
-		CalcolaValutazione( int idStudente, int percentualeAssenze, float mediaVoti) {
+		double mediaVoti;
+		CalcolaValutazione( int idStudente, int percentualeAssenze, double mediaVoti) {
 			this.idStudente = idStudente;
 			this.percentualeAssenze = percentualeAssenze;
 			this.mediaVoti = mediaVoti;
 		}
 		
-		void CalcolaValutazione() {
-			if (percentualeAssenze < 50);{
-				System.out.println("Bocciato");
-			}  if (percentualeAssenze < 50 && percentualeAssenze >= 20 && mediaVoti > 2.0) {
-				System.out.println("Promosso");
-			} else if (percentualeAssenze < 25 && mediaVoti < 2.0) {
-				System.out.println("Promosso");
+		boolean promosso() {
+			boolean promosso;
+			
+			if(percentualeAssenze > 50) {
+				promosso = false;
+			} else if(percentualeAssenze > 25 && percentualeAssenze <= 50) {
+				promosso = mediaVoti > 2.0;
+			} else {
+				promosso = mediaVoti >= 2.0;
 			}
 			
+			return promosso;
 		}
+
 }
--- src/it/corso/valutazioni/Main.java
+++ src/it/corso/valutazioni/Main.java
@@ -1,21 +1,37 @@
 package it.corso.valutazioni;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
-		CalcolaValutazione[] randomCalcolaValutazione = new CalcolaValutazione[20];
+		CalcolaValutazione[] cv = new CalcolaValutazione[20];
 		
-		Random randomGenerator = new Random();
-		int idStudente, percentualeAssenze;
-		float mediaVoti;
-		for(int i= 0; i < randomCalcolaValutazione.length; i++) {
-			idStudente = i+1;
-			percentualeAssenze = randomGenerator.nextInt(100) +1;
-			randomCalcolaValutazione[i] = new CalcolaValutazione(idStudente, percentualeAssenze, mediaVoti);
-			System.out.println("ID Studente: " + randomCalcolaValutazione[i].idStudente + "percentuale assenze: " );
+		Random randomGen = new Random();
+		
+		System.out.println(Arrays.toString(cv));
+		
+		int id;
+		int percAssenze;
+		double mediaVoti;
+		for(int i = 0; i <cv.length; i++) {
+			id = i +1;
+			percAssenze = randomGen.nextInt(101);
+			mediaVoti = randomGen.nextDouble() * 5.0;
+			
+
+			cv[i] = new CalcolaValutazione(id, percAssenze, mediaVoti);
+			System.out.print("Valutazione studente id " + cv[i].idStudente);
+			System.out.print(", percentuale assenze " + cv[i].percentualeAssenze);
+			System.out.println(", media voti " + cv[i].mediaVoti);
+			
+			if(cv[i].promosso()) {
+				System.out.println("Promosso");
+			} else {
+				System.out.println("Bocciato");
+			}
 		}
 		
 	}
